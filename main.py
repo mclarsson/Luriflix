65982c1a685ec5fea37355a5bca4dee85e4c4013
mclarsson <martin.larsson@live.com>
2016-11-01 16:11:58 +0100

    Renamed to interface

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 from scanner import Scanner
-from cli import CLI
+from interface import *
 from fileType import *
 
 class Luriflix:
@@ -15,7 +15,7 @@ class Luriflix:
 	def scan(self, suffixes = []):
 		if len(suffixes) == 0:
 			suffixes = self.file_type.suffixes()
-			
+
 		return self.scanner.scan(suffixes)
 
 if __name__ == "__main__":
