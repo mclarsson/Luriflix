fb486668837a2cf14ead13e0823b3e49acf25f3b
Martin Larsson <martin.larsson@live.com>
2016-11-08 17:40:00 +0100

    Changed listing of files

--- interface.py
+++ interface.py
@@ -103,30 +103,40 @@ class GUI:
 		self.current_list = self.complete_list
 
 		# GUI stuff
+		
+		# Main window
 		self.top = Tk()
-		self.top.minsize(width=500, height=500)
+		self.top.minsize(width = 750, height = 750)
 		self.top.grid_columnconfigure(0, weight=1)
 		self.top.grid_rowconfigure(1, weight=1)
+		self.top.wm_title("Lurifliks")
 
-		self.search_field = Entry(self.top, bd =5)
-		self.search_field.grid(column=0, row=0, sticky='we', columnspan = 2)
-		self.search_field.bind("<Key>", self.search_key_down)
+		# Search field
+		self.search_field = Entry(self.top, borderwidth = 0, foreground = "#222", background = "#eee")
+		self.search_field.grid(column = 0, row = 0, sticky = 'we', columnspan = 2, ipadx = 10, ipady = 10, padx = (10, 0))
+		self.search_field.bind("<KeyRelease>", self.search_key_released)
 
-		self.file_list = Listbox(self.top)
+		# List of files
+		self.file_list = Canvas(self.top, borderwidth = 0, background = "#fff")
+		self.file_list.grid(column = 0, row = 1, sticky = 'wesn')
 		self.list_files()
-		self.file_list.grid(column=0, row=1, sticky='wesn')
 
-		self.scrollbar = Scrollbar(self.top, command=self.file_list.yview)
-		self.scrollbar.config(command=self.file_list.yview)
+		# Scrollbar
+		self.scrollbar = Scrollbar(self.top)
+		self.scrollbar.config(command = self.file_list.yview)
 		self.file_list.config(yscrollcommand = self.scrollbar.set)
-		self.scrollbar.grid(column=1, row=1, sticky='sn')
+		self.scrollbar.grid(column = 1, row = 1, sticky = 'sn')
 
+		# Initiate
 		self.top.mainloop()
 
-	def search_key_down(self, key):
-		value = self.search_field.get() + key.char
-		self.filtered_list = self.lfx.filter(value.split())
-		if len(self.filtered_list) == 0:
+	def search_key_released(self, key):
+		""" Called when a key is pressed in the search field """ 
+		query = self.search_field.get()
+		self.filtered_list = self.lfx.filter(query.split())
+
+		if not self.search_field.get():
+			# Print all if search field is empty
 			self.current_list = self.complete_list
 		else:
 			self.current_list = self.filtered_list
@@ -134,7 +144,14 @@ class GUI:
 		self.list_files()
 
 	def list_files(self):
-		# List of files
-		self.file_list.delete(0, END)
-		for file in self.current_list:
-			self.file_list.insert(END, file.attributes['Title'])
+		""" Print current list of files. """
+
+		self.file_list.delete("all")
+		for i in range(len(self.current_list)):
+			file = self.current_list[i]
+			self.file_list.create_rectangle(0, (i * 90), 80, (i * 90) + 80, fill = "#e0e0e0")
+			self.file_list.create_text(90, (i * 90), text = file.attributes['Title'], anchor = 'nw')
+			self.file_list.create_text(90, (i * 90) + 18, text = file.attributes['File'], anchor = 'nw')
+			self.file_list.create_text(90, (i * 90) + 36, text = file.attributes['Source'], anchor = 'nw')
+
+		self.file_list.configure(scrollregion = self.file_list.bbox("all"))
